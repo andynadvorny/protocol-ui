fd8b8d7c2e0627bc68c420438e447719797304e1
Andy Nadvorny <andynadvorny@gmail.com>
2023-03-29 18:01:03 -0300

    style main and text content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
       <body className="bg-zinc-900">
         <Sidebar />
         
-        <div className="">
+        <div className="ml-80 relative h-screen">
           <Header />
           <HeroPattern />
 
-          {children}
+          <div className="py-24 px-8 max-w-4xl mx-auto">
+            {children}
+          </div>
         </div>
       </body>
     </html>
