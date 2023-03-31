5dff38726ee7198687c926f25d19c7608bd35457
Andy Nadvorny <andynadvorny@gmail.com>
2023-03-31 14:11:09 -0300

    add navlink component

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+interface NavLinkProps {
+  children: string
+  href: string
+}
+
+export function NavLink({ children, href }: NavLinkProps) {
+  const activeHref = usePathname()
+
+  return (
+    <Link 
+      data-active={activeHref === href}
+      className="px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors data-[active=true]:border-cyan-400" 
+      href={href}
+    >
+      {children}
+    </Link>
+  )
+}
